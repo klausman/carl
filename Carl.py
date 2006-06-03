d5b515bf0d93aeec795ebeb90bbd2938099b4a21
Tobias Klausmann <klausman@schwarzvogel.de>
2006-06-03 14:08:21 +0000

    First cleanup touches.
    git-svn-id: file:///home/klausman/SVN/carl/trunk@3 9d3e384f-5215-0410-837b-8649f6d58e7a

--- Carl.py
+++ Carl.py
@@ -28,7 +28,7 @@ if len(sys.argv)>1 and sys.argv[1] in ["-h", "--help"]:
 if len(sys.argv) > 1:
     fn = sys.argv[1]
     try:
-        fd=open(fn, "r")
+        fd = open(fn, "r")
     except Exception, e:
         print "Cannot open %s for reading: %s" % (fn,e.strerror)
         sys.exit(1)
@@ -38,30 +38,30 @@ else:
     fd = sys.stdin
     print "Reading from stdin"
 
-ipc=Account.Account()
-ipb=Account.Account()
-sessions=IPSessions.IPSessions()
+ipc = Account.Account()
+ipb = Account.Account()
+sessions = IPSessions.IPSessions()
 
-linecount=0
-totaltraffic=0L
+linecount = 0
+totaltraffic = 0L
 
 sys.stdout.flush()
 
-rtime=time.time()
+rtime = time.time()
 try:
     for ll in fd:
-        linecount+=1
-        if linecount==1:
+        linecount += 1
+        if linecount == 1:
             # timefmt: 2004/02/23 23:11:27 
             d,t,i,r = ll.split(None, 3)
             start = time.mktime(time.strptime("%s %s"%(d,t), \
                 "%Y/%m/%d %H:%M:%S"))
             
 
-        pid,l=ll[20:].split(" ",1)
+        pid, l = ll[20:].split(" ",1)
         if l.startswith("rsync on gentoo-") and not l.startswith("rsync on gentoo-portage/metadata") and not l.startswith("rsync on gentoo-portage//metadata"):
             try:
-                r,o,m,f,hn,ip=l.split(" ",6)
+                r, o, m, f, hn, ip = l.split(" ",6)
             except:
                 continue
             ipc.incr('%15s (%s)'%(ip[1:-2], hn))
@@ -69,17 +69,18 @@ try:
 
         elif l.startswith("sent"):
             try:
-                w,wbytes,b,E,r,rbytes,b,E,t,s,tbytes=l.split(" ",11)
+                w, wbytes, b, E, r, rbytes, b, E, t, s, tbytes =\
+                    l.split(" ",11)
             except:
                 continue
-            ip=sessions.pop(pid)
+            ip = sessions.pop(pid)
             ipb.incr(ip,int(wbytes)+int(rbytes))
-            totaltraffic+=long(rbytes)+long(wbytes)
+            totaltraffic += long(rbytes)+long(wbytes)
 
     d,t,r = ll.split(None, 2)
     span = (time.mktime(time.strptime("%s %s"%(d,t), \
         "%Y/%m/%d %H:%M:%S"))-start) / (24*3600)
-    rtime=time.time()-rtime
+    rtime = time.time()-rtime
 
     del (fd)
     print "Total traffic: %0.2f GBytes"%(totaltraffic/(1024*1024*1024.0))
@@ -94,13 +95,11 @@ try:
 
     print "------------------------------------------------"
 
-    ttop5num=long(ipb.seencount*0.05)
-    ttop5traffic=0L
+    ttop5num = long(ipb.seencount*0.05)
+    ttop5traffic = 0L
     for e in ipb.counts()[-ttop5num:]:
-        ttop5traffic+=e[0]
+        ttop5traffic += e[0]
 
-#print "Top 5%% of IPs by traffic: %s\nThey account for %s bytes (%0.2fGB) of traffic."%\
-#(ttop5num,ttop5traffic,ttop5traffic/(1024*1024*1024.0))
     print "Top 5%% of IPs (%s) account for %s bytes (%0.2fGB) of traffic."%\
     (ttop5num,ttop5traffic,ttop5traffic/(1024*1024*1024.0))
     print "which is %0.2f%% of the total traffic."%\
@@ -120,10 +119,10 @@ try:
     print "Average number of sessions per day: %0.2f"%(sessions.seencount/span)
     print
 
-    stop5num=long(ipc.seencount*0.05)
-    stop5sessions=0L
+    stop5num = long(ipc.seencount*0.05)
+    stop5sessions = 0L
     for e in ipc.counts()[-stop5num:]:
-        stop5sessions+=e[0]
+        stop5sessions += e[0]
     print "Top 5%% of IPs (%s) account for %s sessions."%\
     (stop5num,stop5sessions)
     print "which is %0.2f%% of the total number of sessions."%\
@@ -134,6 +133,3 @@ try:
 except KeyboardInterrupt:
     sys.stderr.write("Interrupted. Probably your fault.\n")
     sys.exit(1)
-
-        
-        
