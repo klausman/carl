bc1420e8c4ee5d2655a68813c86527673e9064a5
Tobias Klausmann <klausman@schwarzvogel.de>
2007-01-04 15:43:58 +0000

    Major overhaul: - switched to using optparse - added obfuscation option - tune output - addec crunch() function and used it
    git-svn-id: file:///home/klausman/SVN/carl/trunk@20 9d3e384f-5215-0410-837b-8649f6d58e7a

--- Carl.py
+++ Carl.py
@@ -1,16 +1,32 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2003-2007  Tobias Klausmann
+#
 """
-A very simplistic rsync logfile analyzer.
+An rsync logfile analyzer.
 
 Intended to be used mainly by Gentoo Rsync Mirror admins
 """
 import sys
 import time
+import md5
+
+from optparse import OptionParser
 
 import Accounts
 import Sessions
 
 __revision__ = "0.4"
+
+# This script was originally intended for Gentoo rsync
+# mirrors only. If you want, you can tune the rsync module
+# name here. No trailing slashes, no regex or other fancy
+# stuff.
+__MODULE__ = "gentoo-portage"
+
+# I pity the guy who has to add "E" (Exa) to the end of this list
+__SIPREFIXES__ = ["", "k", "M", "G", "T", "P"]
 
 try:
     import psyco
@@ -24,29 +40,59 @@ def usage():
     Print out short usage info.
     """
     print """Usage: %s [logfile]
-       Alternatively, logfile is read from stdin.""" % sys.argv[0]
+       If logfile is not specified, it's read from stdin.""" % sys.argv[0]
+
+def crunch(number, div=1024):
+    """
+    Repeatedly divides a number by div until it is smaller than div 
+    """
+    # Make sure it's a float
+    div *= 1.0
+    passes = 0
+    while number > div:
+        number /= div
+        passes += 1
+    return (number, passes)
+
+
+def ob(s, style):
+    """
+    Return an obfuscated version of the string if the
+    global var style is set to True.
+    """
+    if style == "fancy":
+        return md5.new(s).hexdigest()
+    elif style == "simple":
+        s=s[:s.rfind(".")]
+        s=s[:s.rfind(".")]
+        return "%s.x.x" %(s)
+    else:
+        return s
 
 def main():
     """
     Main program.
     """
+    usage = "usage: %prog [options] [filename]\n\nIf filename is not specified, read from stdin"
+    parser = OptionParser(usage=usage, version="%%prog %s" % __revision__)
+    parser.add_option("-o", "--obfuscation", action="store", type="string",
+                    dest="ostyle", default="none",
+                    help="obfuscation style (simple, fancy, none) [%default]")
+    (options, args) = parser.parse_args()
+
     print "Carl (Carl Analyzes Rsync Logfiles) %s" % __revision__
     print "(C) Tobias Klausmann"
     if __psyco_enabled__:
         print "Psyco found and enabled."
 
-    if len(sys.argv)>1 and sys.argv[1] in ["-h", "--help"]:
-        usage()
-        sys.exit(0)
-
-    if len(sys.argv) > 1:
-        fname = sys.argv[1]
+    if len(args) > 0:
+        fname = args[0]
         try:
             logfd = open(fname, "r")
         except IOError, eobj:
-            print "Cannot open %s for reading: %s" % (fname, eobj.strerror)
+            print "Cannot open '%s' for reading: %s" % (fname, eobj.strerror)
             sys.exit(1)
-        print "Reading from", fname
+        print "Reading from '%s'" %(fname)
 
     else:
         logfd = sys.stdin
@@ -74,15 +120,15 @@ def main():
                 
 
             pid, msg = line[20:].split(" ", 1)
-            if msg.startswith("rsync on gentoo-") and \
-                not msg.startswith("rsync on gentoo-portage/metadata") and \
-                not msg.startswith("rsync on gentoo-portage//metadata"):
+            if msg.startswith("rsync on %s" % (__MODULE__) ) and \
+                not msg.startswith("rsync on %s/metadata" % (__MODULE__)) and \
+                not msg.startswith("rsync on %s//metadata" % (__MODULE__)):
                 try:
                     hname, ipaddr = msg.split(" ", 6)[4:6]
                 except ValueError:
                     continue
-                ipc.incr('%15s (%s)'%(ipaddr[1:-2], hname))
-                sessions.push(pid, '%15s (%s)' % (ipaddr[1:-2], hname))
+                ipc.incr('%s'%(ipaddr[1:-2]))
+                sessions.push(pid, '%s' % (ipaddr[1:-2]))
 
             elif msg.startswith("sent"):
                 try:
@@ -101,44 +147,52 @@ def main():
         rtime = time.time()-rtime
 
         del (logfd)
-        print "Total traffic: %0.2f GBytes" % (totaltraffic/(1024*1024*1024.0))
+
+        # Now, generate the report.
+        #
+        tt, pfxn = crunch(totaltraffic)
+        print "Total traffic: %.2f %sBytes" % (tt, __SIPREFIXES__[pfxn])
+        print "Total number of sessions:", sessions.seencount
         print "Total number of unique IPs:", ipb.seencount
         print "Log seems to span %0.2f days." % (span)
         print
-        print " Top 10 Hosts by bytes"
-        print "     bytes ( MBytes )     IP-Adress   (Hostname)"
-        print "------------------------------------------------"
+        print " Top 10 Hosts by byte count"
+        print "     bytes     ( Bytes )     IP-Address"
+        print "-----------------------------------------"
         for entry in ipb.counts()[-10:]:
-            print "%10s (%7.2fM) %s" % \
-                (entry[0], entry[0]/(1024*1024.0), entry[1])
+            sbytes, pfxn = crunch(entry[0])
+            print "%12s (%9.2f%s) %15s" % \
+                (entry[0], sbytes, __SIPREFIXES__[pfxn], ob(entry[1], options.ostyle))
 
-        print "------------------------------------------------"
+        print "-----------------------------------------"
+        savg, pfxn = crunch(totaltraffic/span)
+        print "Average traffic per day: %0.2f bytes (%0.2f%sB)"% \
+            (totaltraffic/span, savg, __SIPREFIXES__[pfxn])
 
         ttop5num = long(ipb.seencount*0.05)
         ttop5traffic = 0L
         for entry in ipb.counts()[-ttop5num:]:
             ttop5traffic += entry[0]
-
+        
+        stop, pfxn = crunch(ttop5traffic)
         print \
-         "Top 5%% of IPs (%s) account for %s bytes (%0.2fGB) of traffic." % \
-         (ttop5num,ttop5traffic,ttop5traffic/(1024*1024*1024.0))
+         "Top 5%% of IPs (%s) account for %s bytes (%0.2f%sB) of traffic," % \
+         (ttop5num,ttop5traffic,stop,__SIPREFIXES__[pfxn])
         print "which is %0.2f%% of the total traffic."% \
          (ttop5traffic/(totaltraffic/100.0))
 
            
         print
 
-        print " Top 10 Hosts by number of sessions"
-        print "Sessions   per day    IP-Adress   (Hostname)"
-        print "-------------------------------------"
+        print " Top 10 Hosts by session count"
+        print "Sessions   per day    IP-Address"
+        print "----------------------------------"
         for entry in ipc.counts()[-10:]:
-            print "%6s      %5.2f %s"  % (entry[0], entry[0]/span, entry[1])
+            print "%6s    %6.2f   %15s"  % (entry[0], entry[0]/span, ob(entry[1], options.ostyle))
 
-        print "-------------------------------------"
-        print "Total number of sessions..........:", sessions.seencount
+        print "----------------------------------"
         print "Average number of sessions per day: %0.2f" % \
             (sessions.seencount/span)
-        print
 
         stop5num = long(ipc.seencount*0.05)
         stop5sessions = 0L
@@ -155,6 +209,9 @@ def main():
     except KeyboardInterrupt:
         sys.stderr.write("Interrupted. Probably your fault.\n")
         sys.exit(1)
+    except ValueError:
+        sys.stderr.write("Your logfile has a very strange format.\n")
+        sys.exit(2)
 
 if __name__ == "__main__":
     main()
